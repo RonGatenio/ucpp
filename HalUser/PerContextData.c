70cfb0394ce433d5a9e0d0daadc7a1e5b348bfc6
Ron Gatenio <ron.gatenio@gmail.com>
2024-03-12 15:04:50 +0200

    added PCD for user

--- HalUser/PerContextData.c
+++ HalUser/PerContextData.c
@@ -1,35 +1,141 @@
 #include "PerContextData_internal.h"
+#include <Windows.h>
+
+typedef struct _PCD_ENTRY
+{
+	PFN_DATA_RELEASE pfnDataRelease;
+	void* pvData;
+} PCD_ENTRY, * PPCD_ENTRY;
+
+void _cdecl pcd_ReleaseEntry(PPCD_ENTRY ptEntry)
+{
+	if (NULL == ptEntry)
+	{
+		return;
+	}
+
+	if (NULL != ptEntry->pfnDataRelease)
+	{
+		ptEntry->pfnDataRelease(ptEntry->pvData);
+	}
+	std_free(ptEntry);
+}
+
+bool _cdecl pcd_CreateEntry(PPCD_ENTRY* pptEntry)
+{
+	bool bRet = false;
+	PPCD_ENTRY ptEntry = NULL;
+
+	ptEntry = std_malloc(sizeof(*ptEntry));
+	if (NULL == ptEntry)
+	{
+		goto lblCleanup;
+	}
+
+	ptEntry->pfnDataRelease = NULL;
+	ptEntry->pvData = NULL;
+
+	*pptEntry = ptEntry;
+	ptEntry = NULL;
+	bRet = true;
+
+lblCleanup:
+	if (NULL != ptEntry)
+	{
+		pcd_ReleaseEntry(ptEntry);
+		ptEntry = NULL;
+	}
+
+	return bRet;
+}
 
 _Use_decl_annotations_
 bool _cdecl PCD_Create(HPCD* phPcd)
 {
-	// TODO
-	return false;
+	bool bRet = false;
+	DWORD dwTlsIndex = TLS_OUT_OF_INDEXES;
+
+	if ((dwTlsIndex = TlsAlloc()) == TLS_OUT_OF_INDEXES)
+	{
+		goto lblCleanup;
+	}
+
+	*phPcd = (HPCD)dwTlsIndex;
+	dwTlsIndex = TLS_OUT_OF_INDEXES;
+	bRet = true;
+
+lblCleanup:
+	if (TLS_OUT_OF_INDEXES != dwTlsIndex)
+	{
+		TlsFree(dwTlsIndex);
+		dwTlsIndex = TLS_OUT_OF_INDEXES;
+	}
+
+	return bRet;
 }
 
 _Use_decl_annotations_
 void _cdecl PCD_Destroy(HPCD hPcd)
 {
-	// TODO
+	DWORD dwTlsIndex = (DWORD)hPcd;
+
+	if (TLS_OUT_OF_INDEXES != dwTlsIndex)
+	{
+		TlsFree(dwTlsIndex);
+		dwTlsIndex = TLS_OUT_OF_INDEXES;
+	}
 }
 
 _Use_decl_annotations_
 bool _cdecl PCD_GetContext(HPCD hPcd, void** ppvData)
 {
-	// TODO
-	return false;
+	void* data = NULL;
+
+	data = TlsGetValue((DWORD)hPcd);
+	
+	if ((data == NULL) && (GetLastError() != ERROR_SUCCESS)) {
+		return false;
+	}
+
+	*ppvData = data;
+
+	return true;
 }
 
 _Use_decl_annotations_
 bool _cdecl PCD_SetContext(HPCD hPcd, void* pvData, PFN_DATA_RELEASE pfnDataRelease)
 {
-	// TODO
-	return false;
+	bool bRes = false;
+	PPCD_ENTRY ptEntry = NULL;
+
+	bRes = pcd_CreateEntry(&ptEntry);
+	if (!bRes) {
+		goto lblCleanup;
+	}
+
+	ptEntry->pfnDataRelease = pfnDataRelease;
+	ptEntry->pvData = pvData;
+
+	bRes = TlsSetValue((DWORD)hPcd, pvData);
+	if (!bRes) {
+		goto lblCleanup;
+	}
+
+	bRes = true;
+	
+lblCleanup:
+	return bRes;
 }
 
 _Use_decl_annotations_
 bool _cdecl PCD_ReleaseContext(HPCD hPcd)
 {
-	// TODO
-	return false;
+	PPCD_ENTRY ptEntry = NULL;
+
+	if (!PCD_GetContext(hPcd, &ptEntry))
+		return false;
+
+	pcd_ReleaseEntry(ptEntry);
+
+	return true;
 }
